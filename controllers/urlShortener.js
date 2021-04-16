fe52b434c1722c32f0e810aec3513628e62b99d1
Sakshi Bobade <bobadesakshi21@gmail.com>
2021-04-16 16:51:07 +0530

    create a short url and return it

--- controllers/urlShortener.js
+++ controllers/urlShortener.js
@@ -1,11 +1,34 @@
 const shortUrl = require('../models/shortUrl')
 
 exports.urlShortener = async (req, res, next) => {
-  await shortUrl.create({ full: req.body.fullUrl })
+  let urlObj = await shortUrl.findOne({ full: req.body.fullUrl })
 
-  const shortUrlObj = await shortUrl.findOne({ full: req.body.fullUrl })
+  const fullUrl = req.body.fullUrl
+  const customShortUrl = req.body.customShortUrl
+
+  if (!urlObj) {
+    if (customShortUrl) {
+      const customUrl = await shortUrl.findOne({ short: customShortUrl })
+      if (customUrl) {
+        res.status(409).json({})
+      }
+    }
+    urlObj = await shortUrl.create({ full: fullUrl, short: customShortUrl })
+  }
 
   res.status(201).json({
-    shortUrl: shortUrlObj.short
+    shortUrl: urlObj.short
   })
+}
+
+exports.redirectToOrignalUrl = async (req, res, next) => {
+  const shortParam = req.params.shortUrl
+  const urlObj = await shortUrl.findOne({ short: shortParam })
+
+  if (!urlObj) return res.sendStatus(404)
+
+  urlObj.clicks++
+  urlObj.save()
+
+  res.redirect(urlObj.full)
 }
--- routes/urlShortener.js
+++ routes/urlShortener.js
@@ -4,4 +4,6 @@ const urlController = require('../controllers/urlShortener')
 
 router.post('/', urlController.urlShortener)
 
+router.get('/:shortUrl', urlController.redirectToOrignalUrl)
+
 module.exports = router
