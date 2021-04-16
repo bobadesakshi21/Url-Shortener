3c10f3be71a16ea156f563ee7d29e2be6c9a11fa
Sakshi Bobade <bobadesakshi21@gmail.com>
2021-04-16 12:10:56 +0530

    Mongodb connection

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const express = require('express')
 const app = express()
+const mongoose = require('mongoose')
+
+mongoose.connect('mongodb+srv://Sakshi:sakshi123@cluster0.vpzlm.mongodb.net/urlShortener?retryWrites=true&w=majority')
 
 const urlRoute = require('./routes/urlShortener')
 app.use('/url', urlRoute)
