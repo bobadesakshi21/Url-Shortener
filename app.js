bd48ce7dd356fc6b8358086ef6f60ad145d97034
Sakshi Bobade <bobadesakshi21@gmail.com>
2021-04-16 11:05:18 +0530

    Routing boiler plate

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 const express = require('express')
 const app = express()
 
+const urlRoute = require('./routes/urlShortener')
+app.use('/url', urlRoute)
 app.listen(process.env.PORT || 3000)
--- routes/urlShortener.js
+++ routes/urlShortener.js
@@ -1,4 +1,6 @@
 const express = require('express')
-
 const router = express.Router()
+const urlController = require('../controllers/urlShortener')
+
+router.get('/', urlController.urlSortener)
 module.exports = router
