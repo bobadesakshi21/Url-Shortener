38a94472c88a5f2ad1a50f2225fc390adcb05192
Sakshi Bobade <bobadesakshi21@gmail.com>
2021-04-17 11:45:25 +0530

    user login

--- controllers/auth.js
+++ controllers/auth.js
@@ -37,3 +37,60 @@ exports.signUp = async (req, res, next) => {
     next(err)
   }
 }
+exports.login = async (req, res, next) => {
+  // Get the user data from the request body
+  const username = req.body.username
+  const password = req.body.password
+
+  try {
+    // Check if the user exists in db
+    const user = await User.findOne({ username: username })
+    if (!user) {
+      const err = new Error('User not found!')
+      err.statusCode = 401
+      throw err
+    }
+
+    // Check if the password is valid
+    const isEqual = await bcrypt.compare(password, user.password)
+    if (!isEqual) {
+      const err = new Error('Wrong Password')
+      err.statusCode = 401
+      throw err
+    }
+
+    // Generate uuids for access and refresh token
+    const accessTokenId = uuidv4()
+    const refreshTokenId = uuidv4()
+
+    // Generate access token
+    const accessToken = jwt.sign({
+      userId: user.id,
+      sessionId: accessTokenId
+    },
+      config.get('accessToken.secret'),
+      { expiresIn: config.get('accessToken.expiry') })
+
+    // Generate refresh token
+    const refreshToken = jwt.sign({
+      userId: user.id,
+      sessionId: refreshTokenId
+    },
+      config.get('refreshToken.secret'),
+      { expiresIn: config.get('refreshToken.expiry') })
+
+    // Add access and refresh token to the cookie
+    res.cookie('accessToken', accessToken, {
+      expires: new Date(Date.now() + 60 * 60 * 1000) // 1 hr
+    })
+    res.cookie('refreshToken', refreshToken, {
+      expires: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000) // 7 days
+    })
+    // res.status(200).json({
+    //   accessToken,
+    //   refreshToken
+    // })
+  } catch (err) {
+    next(err)
+  }
+}
